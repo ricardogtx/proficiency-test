50413b0ccc663e9b35a0db0dd309fa631dd3822a
ricardogtx <ricardocdzg@hotmail.com>
2016-01-05 10:39:43 -0200

    Add methods of controller

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,2 +1,60 @@
 class CourseController < ApplicationController
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
+
+
+def index
+ @courses = Course.all
+end
+
+def new
+ @course = Course.new
+end
+
+def create
+@course = Course.new(course_params)
+ respond_to do |format|
+   if @course.save
+     format.html { redirect_to @course, notice: 'Curso criado com sucesso.' }
+   else
+     format.html { render :new }
+   end
+ end
+end
+
+def show
+
+end
+
+def edit
+
+end
+
+def update
+ respond_to do |format|
+     if @course.update(course_params)
+       format.html { redirect_to @course, notice: 'Curso atualizado.' }
+     else
+       format.html { render :edit }
+     end
+   end
+end
+
+def destroy
+ @course.destroy
+   respond_to do |format|
+   format.html { redirect_to courses_url, notice: 'Curso excluído com sucesso.' }
+   end
+end
+
+
+private
+
+ def set_course
+   @course= Course.find(params[:id])
+ end
+
+ def course_params
+   params.require(:course).permit(:name, :description, :status)
+ end
+
 end
